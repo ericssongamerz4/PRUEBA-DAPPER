3fe7c3d39a6f1bca2176cb0167769d17f780ce82
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-03-01 13:29:57 -0600

    Add project files.

---
+++ Models/VideoGame.cs
@@ -0,0 +1,16 @@
+﻿namespace PRUEBA_DAPPER.Models
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+
+        public required string Title { get; set; }
+
+        public required string Publisher { get; set; }
+
+        public required string Developer { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
+
